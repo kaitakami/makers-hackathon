18ccce5851fbc4015f6c319c64d4308cdaaee58f
Kai Takami <kaitakami@protonmail.com>
2023-08-26 23:46:02 -0600

    feat: add parallax text to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import ParallaxText from "@/components/ParallaxText"
 import Hero from "@/components/hero"
 
 export default function Home() {
   return (
     <main className="">
-      <Hero />
+      <section className="relative min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-zinc-300 via-zinc-100 to-zinc-300">
+        <Hero />
+      </section>
+      <section className="py-12 font-extrabold">
+        <ParallaxText baseVelocity={-5}>
+          Makers Hackathon
+        </ParallaxText>
+      </section>
+      <section>
+      </section>
     </main>
   )
 }
