3bf0a34d4349dc1adf7c63187ea04b89f4ac50d9
Kai Takami <kaitakami@protonmail.com>
2023-08-26 22:55:05 -0600

    feat: add responsive images

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,7 +10,7 @@ const ABCCameraFontStyled = localFont({
 
 const Hero = () => {
   return (
-    <section className="relative min-h-screen pb-32">
+    <section className="relative min-h-screen">
       <div className="absolute left-0 right-0 flex flex-col justify-center max-w-5xl min-h-screen px-8 mx-auto md:px-2">
         <motion.div
           initial={{ opacity: 0, x: -30 }}
@@ -20,19 +20,39 @@ const Hero = () => {
         >
           <h1 className="sr-only">Makers Hackathon</h1>
           <div className='flex flex-col items-center'>
-            <Image src="/images/gradientisologo.png" alt="Makers logo" width={100} height={"100"} />
+            <Image
+              src="/images/gradientisologo.png"
+              alt="Makers Isologo"
+              width={100}
+              height={100}
+            />
             <h2 className=" text-lg font-semibold">Make your closet dream reality</h2>
           </div>
         </motion.div>
       </div>
-      <div className="flex flex-col justify-around max-w-5xl min-h-screen px-2 mx-auto text-6xl font-black text-transparent select-none sm:text-8xl md:text-9xl text-zinc-500 opacity-20">
+      <div className="flex flex-col justify-around max-w-5xl min-h-screen px-2 mx-auto text-6xl font-black text-transparent select-none sm:text-8xl md:text-9xl sm:opacity-20 opacity-10">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
           className="z-10"
         >
-          <Image className="absolute z-0 object-cover bg-center select-none opacity-70 brightness-75" src={"/images/blacklogo.png"} alt="Anime style background Image for phone" width={600} height={300} />
+          <Image
+            className="absolute z-0 object-cover bg-center select-none opacity-70 brightness-75 pr-16 hidden md:block"
+            src={"/images/blacklogo.png"}
+            alt="Makers Logo"
+            width={600}
+            height={300}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+          <Image
+            className="absolute z-0 object-cover bg-center select-none opacity-70 brightness-75 pr-16 md:hidden"
+            src={"/images/blacklogo.png"}
+            alt="Makers Logo"
+            width={300}
+            height={150}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
         </motion.div>
         <motion.div
           initial={{ opacity: 0 }}
@@ -40,7 +60,7 @@ const Hero = () => {
           transition={{ duration: 0.5, delay: 0.5 }}
           className="z-10"
         >
-          <p className={`${ABCCameraFontStyled.className} text-end drop-shadow-2xl`}>Hackathon</p>
+          <p className={`${ABCCameraFontStyled.className} text-end drop-shadow-2xl text-zinc-500`}>Hackathon</p>
         </motion.div>
       </div>
     </section>
