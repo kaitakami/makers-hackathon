68963790cc6f87987c372ab3b427ebac669090fd
Kai Takami <kaitakami@protonmail.com>
2023-08-26 22:39:50 -0600

    feat: import local font to code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const ABCCameraFont = localFont({
+  src: './fonts/ABCCameraPlainVariable-Trial.ttf',
+})
+
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ABCCameraFont.className}>{children}</body>
     </html>
   )
 }
