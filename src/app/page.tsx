e9c89c2cfe2fdaa75ddefd9a833c9e385a608156
Kai Takami <kaitakami@protonmail.com>
2023-08-28 21:06:14 -0600

    feat: follow clean code practices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,29 @@
 import ParallaxText from "@/components/ParallaxText"
 import Hero from "@/components/hero"
-import { buttonVariants } from "@/components/ui/button"
-import Image from "next/image"
-import Link from "next/link"
+import Cta from "@/components/cta"
+import Timeline from "@/components/timeline"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
-    <main className="">
-      <section className="relative min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-zinc-300 via-zinc-100 to-zinc-300">
-        <Hero />
-      </section>
-      <section className="py-12 font-extrabold">
-        <ParallaxText baseVelocity={-5}>
-          Makers Hackathon
-        </ParallaxText>
-      </section>
-      <section className="min-h-screen flex flex-col justify-center">
-        <div className="gap-8 items-center pb-16 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 lg:px-6">
-          {/* Add image here */}
-          <div className="mt-4 md:mt-0">
-            <h3 className="mb-4 text-4xl tracking-tight font-extrabold">Exclusively built for Makers Fellows.</h3>
-            <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-              Just like that forgotten shirt in your closet, we all have a project but we put it off until it is forgotten in a small space in our brain. Time to turn this project into reality.
-              <br />
-              <br />
-              <b><i>48 hours</i></b>.
-              <br />Starting{" "}
-              <b><i>September 22nd</i></b>.
-              <br />
-              <b><i>Prizes</i></b>
-              {" "}and more coming...
-            </p>
-            <Link href="/#" className={`${buttonVariants({ variant: "outline" })}`}>Join the hackathon</Link>
-          </div>
-          <Image className="md:block hidden" src="https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80" alt="" width="400" height="700" />
-        </div>
-      </section>
-      {/* TODO: Add footer w/ github link & makers website link */}
-    </main>
+    <>
+      <main>
+        <section className="relative min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-zinc-300 via-zinc-100 to-zinc-300">
+          <Hero />
+        </section>
+        <section className="py-12 font-extrabold bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-zinc-300 via-zinc-100 to-zinc-300 md:text-6xl text-3xl shadow-lg">
+          <ParallaxText baseVelocity={-5}>
+            Makers Hackathon
+          </ParallaxText>
+        </section>
+        <section id="join" className="min-h-screen flex flex-col justify-center">
+          <Cta />
+        </section>
+        <section id="timeline" className="flex flex-col justify-center max-w-4xl mx-auto min-h-screen items-center relative">
+          <Timeline />
+        </section>
+      </main>
+      <Footer />
+    </>
   )
 }
